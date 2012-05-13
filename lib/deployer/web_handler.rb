52289d51a17f865e3260830429b5706c501b31c4
Max Horbul <max.horbul@livingsocial.com>
2012-05-13 09:27:55 -0700

    enable logging

--- lib/deployer/web_handler.rb
+++ lib/deployer/web_handler.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 
 class Deployer::WebHandler < Sinatra::Base
+  configure :production, :development do
+    enable :logging
+  end
+
   post "/" do
     EM.defer do
       worker = Deployer::Worker.new(:logger => logger, :projects => Deployer.projects)
